49924e4266fa4dcc80b275090db924b89d1b90da
Priya9Gupta <priyagupta706807@gmail.com>
2023-08-12 13:10:37 +0530

    dev.to clone

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Hello Javascript')
+
+console.log('nice');
+
+num= 39;
+
+console.log(num);
+
+myname = "priya";
+
+console.log(myname);
+
+m = "67";
+
+if(m%2==0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
+
+
